5eac9aca17c66ddfd7ee14b8fb35fc4efe25414b
Ian Smythe <ianbsmythe@googlemail.com>
2024-10-24 22:44:46 +0100

    functionality for starting the game

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -22,6 +21,14 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+
+    cards = [ firstCard, secondCard ]
+    sum = firstCard + secondCard
+
     renderGame()
 }
 
