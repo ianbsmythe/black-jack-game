e422b34f67e6713c57dd24bcead6f18d8f83bbb0
Ian Smythe <ianbsmythe@googlemail.com>
2024-10-28 13:20:15 +0000

    change to currency symbol

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
 
-playerEl.textContent = player.name + ": $" + player.chips
+playerEl.textContent = player.name + ": £" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
